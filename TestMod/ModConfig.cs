5764166472d853c49df8cef69bce0eb4b2e2119c
Florent <eliteman.fm@gmail.com>
2022-09-14 09:57:38 +0200

    Add mod configuration

---
+++ TestMod/ModConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMod
+{
+    class ModConfig
+    {
+        public string RgbaMessageColor { get; set; } = "176, 224, 230, 255";
+
+        public bool DisplayHudMessage { get; set; } = false;
+
+        public int HudMessageSecondLeft { get; set; } = 4;
+    }
+}
--- TestMod/ModEntry.cs
+++ TestMod/ModEntry.cs
@@ -8,12 +8,16 @@ using StardewValley;
 using System.Collections.Generic;
 using StardewValley.Menus;
 using System.ComponentModel.Design;
+using System.Text.RegularExpressions;
 
 namespace TestMod
 {
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
+
+        private ModConfig Config;
+
         /*********
         ** Public methods
         *********/
@@ -21,11 +25,17 @@ namespace TestMod
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
             helper.Events.GameLoop.DayEnding += this.OnDayEnding;
         }
 
+        public int parse(string rgba)
+        {
+            return int.Parse(rgba);
+        }
+
         List<int> experienceLimitPerLevel = new List<int>() { 100, 380, 770, 1300, 2150, 3300, 4800, 6900, 10000, 15000};
-        
+
         /// <summary>The method called after a new day starts.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
@@ -42,9 +52,19 @@ namespace TestMod
                 index++;
             });
 
-            string lineBreak = string.Concat(Enumerable.Repeat(" ", 10));
-            Game1.chatBox.addMessage("Your xp: " + text, Color.PowderBlue);
-            Game1.chatBox.addMessage(lineBreak, Color.Snow);
+            if(this.Config.DisplayHudMessage == false)
+            {
+                string lineBreak = string.Concat(Enumerable.Repeat(" ", 10));
+                string rgba = this.Config.RgbaMessageColor;
+                string[] rgbaList = Regex.Replace(rgba, @"\s", "").Split(",");
+                Game1.chatBox.addMessage("Your xp: " + text, new Color(parse(rgbaList[0]), parse(rgbaList[1]), parse(rgbaList[2]), parse(rgbaList[3]))); // Default Color: 176, 224, 230, 255 //
+                Game1.chatBox.addMessage(lineBreak, Color.White);
+
+            } else
+            {
+                Game1.addHUDMessage(new HUDMessage(text, Color.PowderBlue, this.Config.HudMessageSecondLeft * 1000f, true));
+            }
+            
         }
 
     }
